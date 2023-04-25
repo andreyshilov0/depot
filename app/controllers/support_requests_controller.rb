6f5bb25fb51e259db2e241120ccce94d99845fab
andreyshilov0 <an.schilow@yandex.ru>
2023-04-25 18:44:07 +0300

    Add webpackr and support controller

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -5,5 +5,9 @@ class SupportRequestsController < ApplicationController
   end
 
   def update
+    support_request = SupportRequest.find(params[:id])
+    support_request.update(response: params.require(:support_request)[:response])
+    SupportRequestMailer.respond(support_request).deliver_now
+    redirect_to support_request_path
   end
 end

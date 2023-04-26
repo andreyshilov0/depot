55240c046e6164d00ed160abcce384ee93d65a6f
andreyshilov0 <an.schilow@yandex.ru>
2023-04-26 14:00:26 +0300

    Fix from_address

--- app/mailboxes/support_mailbox.rb
+++ app/mailboxes/support_mailbox.rb
@@ -1,11 +1,13 @@
 class SupportMailbox < ApplicationMailbox
   def process
-    recent_order = Order.where(email: mail.from_address.to_s).
+    binding.pry
+    recent_order = Order.where(email: mail.from.to_s).
                          order('created_at desc').
                          first
+                         
 
     SupportRequest.create!(
-      email:   mail.from_address.to_s,
+      email:   mail.from.to_s,
       subject: mail.subject,
       body:    mail.body.to_s,
       order:   recent_order
